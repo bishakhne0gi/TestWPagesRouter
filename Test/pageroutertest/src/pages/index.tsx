5c7855ccf332df08ba0b7fcff14a8b856dd1464e
bishakhne0gi <bneogi102002@gmail.com>
2024-07-30 21:06:21 +0530

    ok

--- Test/pageroutertest/src/pages/index.tsx
+++ Test/pageroutertest/src/pages/index.tsx
@@ -5,13 +5,13 @@ export async function getServerSideProps({ req }: { req: any }) {
   const ip = forwarded ? forwarded.split(/, /)[0] : req.socket.remoteAddress;
 
   let isFromIndia = false;
-
+  let countryName = "Unknown";
   try {
     const response = await axios.get(`https://ipapi.co/${ip}/json/`);
     const data = response.data;
 
-    if (data.country === "IN") {
-      isFromIndia = true;
+    if (data.country_name) {
+      countryName = data.country_name;
     }
   } catch (error) {
     console.error("Error fetching IP geolocation:", error);
@@ -25,10 +25,11 @@ export async function getServerSideProps({ req }: { req: any }) {
   };
 }
 
-export default function Home({ ip, isFromIndia }: any) {
+export default function Home({ ip, isFromIndia, countryName }: any) {
   return (
     <div>
       <p>Your IP address is: {ip}</p>
+      <p>Your country is: {countryName}</p>
       <p>{isFromIndia ? "You are from India!" : "You are not from India."}</p>
     </div>
   );
